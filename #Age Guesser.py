781eb0b852d523db43dfc631323001999cef779e
Jamie <jhardin101@outlook.com>
2025-08-28 09:21:35 -0600

    Prevented duplicate age guesses from occurring

--- #Age Guesser.py
+++ #Age Guesser.py
@@ -4,11 +4,16 @@ import random
 def AgeGuesser():
     print("I'm going to guess your age!")
     name = input("What is your name? ")
+    guess_list = []
     
     success = False
     while not success:
         guessed_age = random.randint(15,30)
+        while guessed_age in guess_list: 
+            guessed_age = random.randint(15,30)
+
         answer = input(f"Is your age {guessed_age} ? (y/n):")
+        guess_list.append(guessed_age)
 
         if answer == "y" or answer == "Y":
             print(name + f" is {guessed_age} years old.")
@@ -19,6 +24,7 @@ def AgeGuesser():
 
         else:
             print('Please say y or n.')
+        
 
 
 def main():
